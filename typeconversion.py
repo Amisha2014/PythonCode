36c486cdaaea76203b0f8499b56f4cf9b3a8121b
Amisha <josephine.amisha@gmail.com>
2024-11-28 19:10:47 +0530

    Python Code

---
+++ typeconversion.py
@@ -0,0 +1,33 @@
+# Type conversion means changing one type of data into another.
+# For example,turning a number into a string or turning a string into a number
+
+# Implicit Type conversion- Python automatically change the type for You
+# Explicit Type conversion- You tell Python to change the type
+
+num = 200
+num_str = str(num)
+print(num)
+print(num_str)
+print(type(num))
+print(type(num_str))
+
+string1 = "10"
+num1 = int(string1)
+print (string1)
+print (num1)
+print (type(string1))
+print (type(num1))
+
+num2 = 45
+num3 = 65.8
+num4 = num2 + num3
+print (num4)
+print(type(num4))
+
+
+# string5 = "hello "
+# num5 = int(string1)
+# print (string5)
+# print (num5)
+# print (type(string5))
+# print (type(num5))
